aa9e3ef45f27d54de31364dcb48bc93c640ae85c
Kiyuob Jung <kyjung@msu.edu>
2026-01-09 03:19:59 -0500

    v0.11.10

--- specular/__init__.py
+++ specular/__init__.py
@@ -20,7 +20,7 @@ from .optimization import (
     gradient_method
 )
 
-__version__ = "0.11.9"
+__version__ = "0.11.10"
 __license__ = "MIT"
 __author__ = "Kiyuob Jung"
 __email__ = "kyjung@msu.edu"
